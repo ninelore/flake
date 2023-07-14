8e1d2a56637e460508cec5f4233593aff315f091
ninelore <ninelore@protonmail.com>
2023-07-15 01:02:14 +0200

    progress

--- machine-lillipup.nix
+++ machine-lillipup.nix
@@ -8,8 +8,14 @@
   hardware.opengl.extraPackages = with pkgs; [
     intel-media-driver
   ];
+
   hardware.firmware = with pkgs; [
     sof-firmware
+  ];
+
+  environment.systemPackages = with pkgs; [
+    sof-firmware
+    alsa-ucm-conf
   ];
 
 }
