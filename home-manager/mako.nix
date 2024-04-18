ddd971eb70f63011ea3ccef368a9a648013d9d95
Ingo Reitz <9l@9lo.re>
2024-04-18 17:50:12 +0200

    mako fixes

--- home-manager/mako.nix
+++ home-manager/mako.nix
@@ -4,9 +4,9 @@
 }: {
   services.mako = {
     font = "NotoSansNerdFont-Regular 11";
-    border-radius = 7;
-    background-color = "#151515DD";
-    border-color = "#FAFAFADD";
+    borderRadius = 7;
+    backgroundColor = "#151515DD";
+    borderColor = "#FAFAFADD";
     height = 400;
     width = 550;
   };
