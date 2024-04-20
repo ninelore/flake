44ddfcd473ef7e7052dc7b16a274c4952e374b44
ninelore <9l@9lo.re>
2024-04-20 10:58:49 +0200

    git signing

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -1,17 +1,28 @@
 let
   email = "9l@9lo.re";
   name = "ninelore";
-in {
+  editor = "nvim";
+in
+{
   programs.git = {
     enable = true;
     extraConfig = {
       color.ui = true;
-      core.editor = "nvim";
-      credential.helper = "store";
+      core.editor = editor;
+      #credential.helper = "store";
       github.user = name;
       push.autoSetupRemote = true;
     };
+    signing = {
+      signByDefault = true;
+      key = "794BE2582FB7A351";
+    };
     userEmail = email;
     userName = name;
+  };
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper.enable = true;
+    settings.editor = editor;
   };
 }
