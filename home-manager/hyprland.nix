1cde034d000de617c9bc9601fd2bb82761d7ccf3
ninelore <9l@9lo.re>
2024-04-30 20:36:13 +0200

    adjust hyprprodmode

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -35,11 +35,11 @@ let
   '';
 
   hyprprodmode = pkgs.writeShellScript "hyprprodmode" ''
-    HYPRPRODMODE=$(hyprctl getoption general:gaps_in | awk 'NR==1{print $3}')
+    HYPRPRODMODE=$(hyprctl getoption decoration:rounding | awk 'NR==1{print $2}')
     if [ ! "$HYPRPRODMODE" = 0 ] ; then
       hyprctl --batch "\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
+        keyword general:gaps_in 1;\
+        keyword general:gaps_out 2;\
         keyword decoration:rounding 0;"
       exit
     fi
