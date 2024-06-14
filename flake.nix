861668686b99fedc771e65cfe18e6e3480e4d929
Ingo Reitz <9l@9lo.re>
2024-06-15 00:16:07 +0200

    nixos-hardware: use fork cus breaking in nixpkgs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:K900/nixos-hardware/drop-amdvlk"; #"github:NixOS/nixos-hardware/master";
     nix-alien.url = "github:thiagokokada/nix-alien";
 
     home-manager = {
