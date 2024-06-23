950ab7c5a659f80fec22490fb8c26a06ace3a3b8
Ingo Reitz <9l@9lo.re>
2024-06-23 02:14:03 +0200

    nixos-hardware input back to upstream

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-small.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/drop-amdvlk"; #"github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
     nix-alien.url = "github:thiagokokada/nix-alien";
 
     home-manager = {
