25605b85e0a3c30d1096939e90c428003ca125f8
Ingo Reitz <9l@9lo.re>
2024-09-10 23:19:16 +0200

    ucm overlay v2

--- hardware/common/cros-ucm.nix
+++ hardware/common/cros-ucm.nix
@@ -1,35 +1,30 @@
 { pkgs, ... }:
-let
-  cros-ucm =
-    with pkgs;
-    alsa-ucm-conf.overrideAttrs {
-      wttsrc = fetchFromGitHub {
-        owner = "WeirdTreeThing";
-        repo = "alsa-ucm-conf-cros";
-        rev = "f7be751655e4298851615bded7adaf364ccfb8c3";
-        hash = "sha256-x4DQoYIF8tRlNQ1/vKgTtgzach/CCNYzsl+gxviSVHs=";
-      };
-
-      patches = [ ];
-
-      unpackPhase = ''
-        runHook preUnpack
-        tar xf "$src"
-        runHook postUnpack
-      '';
-
-      installPhase = ''
-        runHook preInstall
-        mkdir -p $out/share/alsa
-        cp -r alsa-ucm*/ucm2 $out/share/alsa
-        runHook postInstall
-      '';
-    };
-in
 {
   nixpkgs.overlays = [
-    (self: super: {
-      alsa-ucm-conf = cros-ucm;
+    (final: prev: {
+      alsa-ucm-conf = prev.alsa-ucm-conf.overrideAttrs {
+        wttsrc = pkgs.fetchFromGitHub {
+          owner = "WeirdTreeThing";
+          repo = "alsa-ucm-conf-cros";
+          rev = "f7be751655e4298851615bded7adaf364ccfb8c3";
+          hash = "sha256-x4DQoYIF8tRlNQ1/vKgTtgzach/CCNYzsl+gxviSVHs=";
+        };
+
+        patches = [ ];
+
+        unpackPhase = ''
+          runHook preUnpack
+          tar xf "$src"
+          runHook postUnpack
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/share/alsa
+          cp -r alsa-ucm*/ucm2 $out/share/alsa
+          runHook postInstall
+        '';
+      };
     })
   ];
 
