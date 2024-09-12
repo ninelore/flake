0b4156b976d573f6a4bfd257a0d829ef914f3f19
Ingo Reitz <9l@9lo.re>
2024-09-12 12:44:00 +0200

    ad crosec-related packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,4 +4,6 @@
   ytmdesktop = pkgs.callPackage ./ytmdesktop { };
   plymouth-bgrt-luks = pkgs.callPackage ./plymouth-bgrt-luks { };
   alsa-ucm-conf-cros = pkgs.callPackage ./alsa-ucm-conf-cros { };
+  cros-ectool = pkgs.callPackage ./cros-ectool { };
+  #cros-gsctool = pkgs.callPackage ./cros-gsctool { }; # Broken
 }
