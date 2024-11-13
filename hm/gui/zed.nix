ff463a08e1858ca2c68c8eadfc75065d8e4124ac
Ingo Reitz <9l@9lo.re>
2024-11-13 13:14:55 +0100

    zed config

--- hm/gui/zed.nix
+++ hm/gui/zed.nix
@@ -3,5 +3,42 @@
   programs.zed-editor = {
     enable = true;
     package = pkgs.pkgs-small.zed-editor;
+    userSettings = {
+      theme = "Zedokai";
+      telemetry = {
+        "diagnostics" = false;
+        "metrics" = false;
+      };
+      "base_keymap" = "VSCode";
+      "assistant" = {
+        "version" = "2";
+        "default_model" = {
+          "provider" = "ollama";
+          "model" = "qwen2.5-coder:1.5b";
+        };
+      };
+    };
+    extensions = [
+      "cargo-tom"
+      "devicetree"
+      "docker-compose"
+      "dockerfile"
+      "ini"
+      "java"
+      "kdl"
+      "latex"
+      "log"
+      "make"
+      "markdown-oxide"
+      "marksman"
+      "mermaid"
+      "meson"
+      "nginx"
+      "nix"
+      "nu"
+      "scss"
+      "sql"
+      "zedokai"
+    ];
   };
 }
