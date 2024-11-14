e7340b62d85711be168c97b472260bb94c286b58
Ingo Reitz <9l@9lo.re>
2024-11-15 00:22:48 +0100

    update flake

--- hm/gui/zed.nix
+++ hm/gui/zed.nix
@@ -2,7 +2,7 @@
 {
   programs.zed-editor = {
     enable = true;
-    package = pkgs.zed-editor_git;
+    #package = pkgs.zed-editor_git;
     userSettings = {
       theme = "Zedokai";
       telemetry = {
