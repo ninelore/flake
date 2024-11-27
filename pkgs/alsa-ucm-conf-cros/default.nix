1a280bc88b5e01502d77b58858603639a2aee86a
Ingo Reitz <9l@9lo.re>
2024-11-27 13:34:24 +0100

    update ucm

--- pkgs/alsa-ucm-conf-cros/default.nix
+++ pkgs/alsa-ucm-conf-cros/default.nix
@@ -4,8 +4,8 @@ alsa-ucm-conf.overrideAttrs {
   wttsrc = fetchFromGitHub {
     owner = "WeirdTreeThing";
     repo = "alsa-ucm-conf-cros";
-    rev = "f7be751655e4298851615bded7adaf364ccfb8c3";
-    hash = "sha256-x4DQoYIF8tRlNQ1/vKgTtgzach/CCNYzsl+gxviSVHs=";
+    rev = "00b399ed00930bfe544a34358547ab20652d71e3";
+    hash = "sha256-lRrgZDb3nnZ6/UcIsfjqAAbbSMOkP3lBGoGzZci+c1k=";
   };
 
   postInstall = ''
