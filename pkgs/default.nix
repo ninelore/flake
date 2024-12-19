f35e32735044e6d73f65b546ca6ec2489c6c1e7a
Ingo Reitz <9l@9lo.re>
2024-12-19 01:39:48 +0100

    drop eshare
    upstream update is a giant shitshow

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 with pkgs;
 {
-  eshare = callPackage ./eshare { };
   plymouth-bgrt-luks = callPackage ./plymouth-bgrt-luks { };
   adwaita-kvantum = callPackage ./adwaita-kvantum { };
   alsa-ucm-conf-cros = callPackage ./alsa-ucm-conf-cros { };
