574a33396b1cf0d3cf4896a50b4cb8faf0bc66ae
Ingo Reitz <9l@9lo.re>
2025-01-06 17:16:23 +0100

    gnome mutter triple buffering patch

--- nix/default.nix
+++ nix/default.nix
@@ -3,7 +3,30 @@
   nixpkgs = {
     config = import ./config.nix;
     overlays = [
-      #(final: prev: { })
+      (final: prev: {
+        mutter = prev.mutter.overrideAttrs (oldAttrs: {
+          src = pkgs.fetchFromGitLab {
+            domain = "gitlab.gnome.org";
+            owner = "vanvugt";
+            repo = "mutter";
+            rev = "triple-buffering-v4-47";
+            sha256 = "sha256-1VXEzKwzrqLCZby2oWxjclA08kPhxs/Om5N17qYeglM=";
+          };
+          preConfigure =
+            let
+              gvdb = final.fetchFromGitLab {
+                domain = "gitlab.gnome.org";
+                owner = "GNOME";
+                repo = "gvdb";
+                rev = "2b42fc75f09dbe1cd1057580b5782b08f2dcb400";
+                hash = "sha256-CIdEwRbtxWCwgTb5HYHrixXi+G+qeE1APRaUeka3NWk=";
+              };
+            in
+            ''
+              cp -a "${gvdb}" ./subprojects/gvdb
+            '';
+        });
+      })
       # Custom packages
       (final: prev: import ../pkgs { pkgs = prev.pkgs; })
     ];
