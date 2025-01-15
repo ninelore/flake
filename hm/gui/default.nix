af2785af8f80d23724650d2573ec66dd0c5db402
Ingo Reitz <9l@9lo.re>
2025-01-15 05:08:54 +0100

    hm/gui/default: restructure

--- hm/gui/default.nix
+++ hm/gui/default.nix
@@ -5,6 +5,53 @@
     ./gnome.nix
   ];
 
+  home = {
+    packages = with pkgs; [
+      # GUI Apps
+      android-studio
+      appimage-run
+      discord-canary
+      fractal
+      gaphor
+      gimp
+      helvum
+      jetbrains.clion
+      jetbrains.datagrip
+      jetbrains.dataspell
+      jetbrains.idea-ultimate
+      libreoffice-fresh
+      obsidian
+      protonvpn-gui
+      scrcpy
+      textpieces
+      via
+      wl-clipboard
+
+      # gayming
+      wineWowPackages.stagingFull
+      bottles
+      prismlauncher
+      (pkgs.retroarch.withCores (
+        cores: with cores; [
+          melonds
+          desmume
+          pcsx2
+          ppsspp
+          vba-m
+        ]
+      ))
+    ];
+    sessionVariables = {
+      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+      GDK_BACKEND = "wayland,x11";
+      MOZ_ENABLE_WAYLAND = 1;
+      NIXOS_OZONE_WL = "1";
+      QT_AUTO_SCREEN_SCALE_FACTOR = 1;
+      QT_ENABLE_HIGHDPI_SCALING = 1;
+      QT_QPA_PLATFORM = "wayland;xcb";
+    };
+  };
+
   programs = {
     alacritty = {
       enable = true;
@@ -54,52 +101,5 @@
     obs-studio = {
       enable = true;
     };
-  };
-
-  home = {
-    sessionVariables = {
-      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-      GDK_BACKEND = "wayland,x11";
-      MOZ_ENABLE_WAYLAND = 1;
-      NIXOS_OZONE_WL = "1";
-      QT_AUTO_SCREEN_SCALE_FACTOR = 1;
-      QT_ENABLE_HIGHDPI_SCALING = 1;
-      QT_QPA_PLATFORM = "wayland;xcb";
-    };
-    packages = with pkgs; [
-      # GUI Apps
-      android-studio
-      appimage-run
-      discord-canary
-      fractal
-      gaphor
-      gimp
-      helvum
-      jetbrains.clion
-      jetbrains.datagrip
-      jetbrains.dataspell
-      jetbrains.idea-ultimate
-      libreoffice-fresh
-      obsidian
-      protonvpn-gui
-      scrcpy
-      textpieces
-      via
-      wl-clipboard
-
-      # gayming
-      wineWowPackages.stagingFull
-      bottles
-      prismlauncher
-      (pkgs.retroarch.withCores (
-        cores: with cores; [
-          melonds
-          desmume
-          pcsx2
-          ppsspp
-          vba-m
-        ]
-      ))
-    ];
   };
 }
