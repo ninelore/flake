90e8a4635c629ba69c9d76e0af4936a55250d80e
Ingo Reitz <9l@9lo.re>
2025-01-21 17:45:52 +0100

    sys/locale: define locales to install

--- sys/locale.nix
+++ sys/locale.nix
@@ -1,6 +1,12 @@
 {
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.supportedLocales = [
+    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "en_GB.UTF-8/UTF-8"
+    "de_DE.UTF-8/UTF-8"
+  ];
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
