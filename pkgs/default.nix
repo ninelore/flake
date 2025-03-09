153538e5f2fb2e284b32475d097968fd040a39da
Ingo Reitz <9l@9lo.re>
2025-03-10 00:04:57 +0100

    pkgs: dont support or build chrultrabook-tools for darwin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,12 @@ with inputs.nixpkgs.legacyPackages.${system};
   plymouth-bgrt-luks = callPackage ./plymouth-bgrt-luks { };
   adwaita-kvantum = callPackage ./adwaita-kvantum { };
   alsa-ucm-conf-cros = callPackage ./alsa-ucm-conf-cros { };
-  chrultrabook-tools = inputs.chrultrabook-tools.packages.${system}.default;
+  chrultrabook-tools = inputs.chrultrabook-tools.packages.${system}.default.overrideAttrs {
+    meta.platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+  };
   cros-ectool = callPackage ./cros-ectool { };
   #cros-gsctool = callPackage ./cros-gsctool { }; # Broken
 }
