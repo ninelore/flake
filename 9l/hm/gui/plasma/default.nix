9eaf5b5445444125e19cdf95a5ff22cc6ffe44c9
Ingo Reitz <9l@9lo.re>
2025-04-09 14:22:12 +0200

    9l: hm: plasma: dont manage panels for now

--- 9l/hm/gui/plasma/default.nix
+++ 9l/hm/gui/plasma/default.nix
@@ -32,53 +32,54 @@
       colorScheme = "MonokaiPro";
     };
 
-    panels = [
-      # Dock
-      {
-        floating = true;
-        height = 56;
-        hiding = "dodgewindows";
-        lengthMode = "fit";
-        location = "bottom";
-        screen = "all";
-        widgets = [
-          {
-            iconTasks = {
-              launchers = [
-                "applications:kitty.desktop"
-                "applications:firefox.desktop"
-                "applications:discord-canary.desktop"
-                "applications:spotify.desktop"
-                "applications:org.gnome.Fractal.desktop"
-                "applications:dev.geopjr.Tuba.desktop"
-              ];
-            };
-          }
-        ];
-      }
-      {
-        floating = false;
-        height = 28;
-        hiding = "none";
-        lengthMode = "fill";
-        location = "top";
-        screen = "all";
-        widgets = [
-          "org.kde.plasma.kickoff"
-          "org.kde.plasma.windowlist"
-          "org.kde.plasma.panelspacer"
-          {
-            digitalClock = {
-              calendar.firstDayOfWeek = "monday";
-              date.format = "isoDate";
-              date.position = "besideTime";
-              time.format = "24h";
-            };
-          }
-          "org.kde.plasma.panelspacer"
-          "org.kde.plasma.systemtray"
-        ];
-      }
-    ];
+    # panels = [
+    #   # Dock
+    #   {
+    #     floating = true;
+    #     height = 56;
+    #     hiding = "dodgewindows";
+    #     lengthMode = "fit";
+    #     location = "bottom";
+    #     screen = "all";
+    #     widgets = [
+    #       {
+    #         iconTasks = {
+    #           launchers = [
+    #             "applications:kitty.desktop"
+    #             "applications:firefox.desktop"
+    #             "applications:discord-canary.desktop"
+    #             "applications:spotify.desktop"
+    #             "applications:org.gnome.Fractal.desktop"
+    #             "applications:dev.geopjr.Tuba.desktop"
+    #           ];
+    #         };
+    #       }
+    #     ];
+    #   }
+    #   # Top bar
+    #   {
+    #     floating = false;
+    #     height = 28;
+    #     hiding = "none";
+    #     lengthMode = "fill";
+    #     location = "top";
+    #     screen = "all";
+    #     widgets = [
+    #       "org.kde.plasma.kickoff"
+    #       "org.kde.plasma.windowlist"
+    #       "org.kde.plasma.panelspacer"
+    #       {
+    #         digitalClock = {
+    #           calendar.firstDayOfWeek = "monday";
+    #           date.format = "isoDate";
+    #           date.position = "besideTime";
+    #           time.format = "24h";
+    #         };
+    #       }
+    #       "org.kde.plasma.panelspacer"
+    #       "org.kde.plasma.systemtray"
+    #     ];
+    #   }
+    # ];
   };
 }
