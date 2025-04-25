7e3fda7305273627e627ab47702c7a1a6db9bd48
Ingo Reitz <9l@9lo.re>
2025-04-25 14:29:21 +0200

    images: fix getty text

--- lib/images.nix
+++ lib/images.nix
@@ -57,6 +57,15 @@ let
             apt
             arch-install-scripts
           ];
+        services.getty.helpLine = lib.mkForce ''
+          The "nixos" and "root" accounts have empty passwords.
+
+          To log in over ssh you must set a password for either "nixos" or "root"
+          with `passwd` (prefix with `sudo` for "root"), or add your public key to
+          /home/nixos/.ssh/authorized_keys or /root/.ssh/authorized_keys.
+
+          If you need a wireless connection, use NetworkManager (nmcli or nmtui).
+        '';
       }
     )
   ];
