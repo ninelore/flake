ac2ff2eaf9b0fa4935209362c5f45230a5753a2d
Ingo Reitz <9l@9lo.re>
2025-05-21 16:12:56 +0200

    9l: hm: drop GDK_BACKEND env var

--- 9l/hm/gui/default.nix
+++ 9l/hm/gui/default.nix
@@ -33,7 +33,6 @@
       ];
     sessionVariables = {
       ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-      GDK_BACKEND = "wayland,x11";
       MOZ_ENABLE_WAYLAND = 1;
       NIXOS_OZONE_WL = "1";
       QT_AUTO_SCREEN_SCALE_FACTOR = 1;
