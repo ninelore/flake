acf54e0eebbe2f384b29cd30d823394a597bfed9
Ingo Reitz <9l@9lo.re>
2025-06-15 05:03:45 +0200

    9l: nix-scripts: add `hm`

--- 9l/hm/cli/nix-scripts.nix
+++ 9l/hm/cli/nix-scripts.nix
@@ -16,6 +16,21 @@ let
     run0 nixos-rebuild --flake $_p $*
   '';
 
+  hm = pkgs.writeShellScriptBin "hm" ''
+    _p=.
+    if [[ -r $HOME/.nx-flakepath ]] && ! [[ -r ./flake.nix ]]; then
+      if [[ -r "$(cat "$HOME"/.nx-flakepath)/flake.nix" ]]; then
+        _p=$(cat "$HOME"/.nx-flakepath)
+        cd "$_p" || exit 1
+        echo "Found flakepath $_p"
+      fi
+    fi
+    if ping -c1 github.com > /dev/null 2>&1; then
+      git pull --ff-only --autostash || exit 1
+    fi
+    ${pkgs.home-manager}/bin/home-manager $_p $*
+  '';
+
   nx-flakepath-update = pkgs.writeShellScriptBin "nx-flakepath-update" ''
     echo "checking flake validity..."
     if [[ -r ./flake.nix ]]; then
@@ -32,6 +47,7 @@ let
 in
 {
   home.packages = [
+    hm
     nxr
     nx-flakepath-update
     spawnb
