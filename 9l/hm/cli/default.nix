8dd2e9fdba15da83fd834efa6a7a34e476ea4076
Ingo Reitz <9l@9lo.re>
2025-07-05 13:31:43 +0200

    9l: hm: edit ghostty keybinds

--- 9l/hm/cli/default.nix
+++ 9l/hm/cli/default.nix
@@ -1,7 +1,4 @@
-{
-  pkgs,
-  ...
-}:
+{ ... }:
 {
   home.stateVersion = "24.05";
 
@@ -31,8 +28,27 @@
         window-theme = "ghostty";
         adw-toolbar-style = "flat";
         gtk-titlebar-hide-when-maximized = true;
-        keybind = "ctrl+,=unbind";
+        keybind = [
+          "performable:ctrl+shift+c=copy_to_clipboard"
+          "performable:ctrl+shift+v=paste_from_clipboard"
+          "ctrl+shift+,=reload_config"
+          "ctrl+shift+enter=new_split:right"
+          "ctrl+shift+o=new_split:down"
+          "ctrl+shift+t=new_tab"
+          "ctrl+shift+equal=increase_font_size:1"
+          "ctrl+shift+plus=increase_font_size:1"
+          "ctrl+shift+minus=decrease_font_size:1"
+          "ctrl+shift+backspace=reset_font_size"
+          "ctrl+shift+w=close_tab"
+          "ctrl+shift+p=toggle_command_palette"
+          "ctrl+shift+arrow_down=goto_split:down"
+          "ctrl+shift+arrow_left=goto_split:left"
+          "ctrl+shift+arrow_right=goto_split:right"
+          "ctrl+shift+arrow_up=goto_split:up"
+          "ctrl+shift+tab=next_tab"
+        ];
       };
+      clearDefaultKeybinds = true;
     };
   };
 
