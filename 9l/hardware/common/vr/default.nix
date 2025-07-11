0b1f18233f306800e3049572c478f68f622db2f5
Ingo Reitz <9l@9lo.re>
2025-07-11 11:01:47 +0200

    9l: monado config

--- 9l/hardware/common/vr/default.nix
+++ 9l/hardware/common/vr/default.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 {
-  services.monado = {
-    enable = true;
-    defaultRuntime = true;
+  services = {
+    monado = {
+      enable = true;
+      defaultRuntime = true;
+    };
+    udev.packages = with pkgs; [ xr-hardware ];
   };
   systemd.user.services.monado.environment = {
-    #STEAMVR_LH_ENABLE = "1";
+    STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
-    AMD_VULKAN_ICD = "RADV";
+    # XRT_COMPOSITOR_FORCE_WAYLAND_DIRECT = "1";
+    # AMD_VULKAN_ICD = "RADV";
   };
 }
