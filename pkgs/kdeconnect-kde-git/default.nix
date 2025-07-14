771e41ba01d7132b7c55dcfd1330c628d180782c
Ingo Reitz <9l@9lo.re>
2025-07-14 08:57:35 +0200

    pkgs: kdeconnect-kde-git: ? -> 25.07.80-unstable-2025-07-14

--- pkgs/kdeconnect-kde-git/default.nix
+++ pkgs/kdeconnect-kde-git/default.nix
@@ -1,7 +1,8 @@
 { kdePackages, ... }:
 kdePackages.kdeconnect-kde.overrideAttrs (prevPdAttrs: {
+  version = "25.07.80-unstable-2025-07-14";
   src = builtins.fetchGit {
     url = "https://invent.kde.org/network/kdeconnect-kde.git";
-    rev = "83ff7010e1373c427f5b491ab0d609849d3ae509";
+    rev = "4ab87318e7589d2d1d28a0c0c43936407b636cdd";
   };
 })
