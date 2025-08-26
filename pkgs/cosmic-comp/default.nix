731cb6e7f5608a85f22a5dbe6c3be850612ad907
Ingo Reitz <9l@9lo.re>
2025-08-26 19:02:13 +0200

    pkgs: cosmic-comp: 1.0.0-alpha.7-unstable-2025-06-26 -> 1.0.0-alpha.7-unstable-2025-08-26

--- pkgs/cosmic-comp/default.nix
+++ pkgs/cosmic-comp/default.nix
@@ -5,15 +5,15 @@
   ...
 }:
 cosmic-comp.overrideAttrs (oldAttrs: rec {
-  version = "1.0.0-alpha.7-unstable-2025-06-26";
+  version = "1.0.0-alpha.7-unstable-2025-08-26";
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "cosmic-comp";
-    rev = "17d6edb6d521563f21b3ed1d6911f1a073a71808";
-    hash = "sha256-9Et7yBlR3vjRHqIRhOjrNOiBb5rLlxx97UQ2qv4WUTw=";
+    rev = "2ccdb6c93d50a727fc8b00f3010b417048dbbffa";
+    hash = "sha256-hj5gYpLdZWkRH0tvgZMgJBMNIteZ/tg/WKvTxKLpltw=";
   };
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit src;
-    hash = "sha256-H2u6gg45LR9PeWPFL8MFemrurSCFiH1lqV5DOWTUSyk=";
+    hash = "sha256-wGq7Cdq56Hnzww+IVXCtZet4Zojj8VUmmsjsg53XHe8=";
   };
 })
