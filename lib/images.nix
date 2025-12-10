472267206628d07d804fe5d004ab5efadb9d0b6e
Ingo Reitz <9l@9lo.re>
2025-12-10 22:08:14 +0100

    images: drop deprecated substituters and public-key

--- lib/images.nix
+++ lib/images.nix
@@ -20,14 +20,12 @@ let
           ];
           substituters = [
             "https://nix-community.cachix.org/"
-            "https://chaotic-nyx.cachix.org/"
             "https://cache.nixos.org/"
             "https://9lore.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
             "9lore.cachix.org-1:H2/a1Wlm7VJRfJNNvFbxtLQPYswP3KzXwSI5ROgzGII="
           ];
         };
