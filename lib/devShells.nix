e414ec945e4359616543208f3d0270290e173c60
Ingo Reitz <9l@9lo.re>
2026-01-18 02:08:47 +0100

    devShells: simplify

--- lib/devShells.nix
+++ lib/devShells.nix
@@ -11,20 +11,26 @@ with pkgs;
   };
 }
 // lib.optionalAttrs (stdenv.isLinux) {
-  kernelBuild = mkShell {
+  kernelDev = mkShell {
     name = "kernel-dev";
     buildInputs = [
       # Tools
       clang-tools
       dtc
+      ubootTools
+      lz4
       # Linux deps
       ncurses
       pkg-config
+      # Cross compilers
+      pkgsCross.aarch64-multiplatform.stdenv.cc
+      pkgsCross.gnu64.stdenv.cc
+      (lib.hiPrio gcc)
     ]
     ++ linux.nativeBuildInputs;
     PKG_CONFIG_PATH = "${ncurses}/lib/pkgconfig";
   };
-  kernelBuildClang = mkShell.override { stdenv = clangStdenv; } {
+  kernelDevClang = mkShell.override { stdenv = clangStdenv; } {
     name = "kernel-dev-clang";
     buildInputs = [
       ncurses
@@ -49,13 +55,12 @@ with pkgs;
       coreboot-toolchain.aarch64
       coreboot-toolchain.i386
       coreboot-toolchain.x64
-      # coreboot-toolchain.riscv
       ncurses
       openssh
       openssl
       pkg-config
       # edk2
-      gcc14
+      gcc14 # Required by OpensslLib
       imagemagick
       libuuid
       python3
@@ -64,29 +69,4 @@ with pkgs;
       unset STRIP
     '';
   };
-}
-// lib.optionalAttrs (stdenv.hostPlatform.system == "x86_64-linux") {
-  crossArm64 = mkShell {
-    name = "generic-cross-arm64";
-    buildInputs = [
-      # Tools
-      clang-tools
-      dtc
-      ubootTools
-      lz4
-      # Linux deps
-      ncurses
-      pkg-config
-      # Compiler
-      pkgsCross.aarch64-multiplatform.stdenv.cc
-      pkgsCross.aarch64-multiplatform-musl.stdenv.cc
-      (lib.hiPrio gcc)
-    ]
-    ++ linux.nativeBuildInputs;
-    PKG_CONFIG_PATH = "${ncurses}/lib/pkgconfig";
-    # CROSS_COMPILE = "aarch64-unknown-linux-gnu-";
-    CROSS_COMPILE = "aarch64-unknown-linux-musl-";
-    ARCH = "arm64";
-  };
-
 }
