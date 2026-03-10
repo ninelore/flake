88df3ec750b10090a16b7ea0e779752f0632b315
Ingo Reitz <9l@9lo.re>
2026-03-10 18:39:43 +0100

    pkgs: drop linux_nix_cros, linux_sc7180

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -12,10 +12,7 @@ with import inputs.nixpkgs {
   depthcharge-tools = callPackage ./depthcharge-tools { };
   # fiedka = callPackage ./fiedka { }; # WIP
   # ghidra-server = callPackage ./ghidra-server { }; # Broken
-  #iosevka-9l = callPackage ./iosevka-9l { }; # Broken
   linux_cros_latest = callPackage ./linux_cros_latest { };
-  #linux_nix_cros = callPackage ./linux_nix_cros { }; # Unfinished
-  linux_sc7180_legacy = callPackage ./linux_sc7180 { };
   plymouth-bgrt-luks = callPackage ./plymouth-bgrt-luks { };
   scripts-9l = callPackage ./scripts-9l { };
   submarine = callPackage ./submarine { };
@@ -24,6 +21,7 @@ with import inputs.nixpkgs {
   chrultrabook-tools = throw "chrultrabook-tools has been removed in favor of the upstream flake";
   linux_mt81 = throw "'linux_mt81' has been removed in favor of 'linux_cros_latest'";
   linux_cros = throw "'linux_cros' has been removed in favor of 'linux_cros_latest'";
-  linux_sc7180 = throw "'linux_sc7180' has been deprecated in favor of 'linux_cros'. It is available as 'linux_sc7180_legacy'";
+  linux_sc7180 = throw "'linux_sc7180' has been removed in favor of 'linux_latest'";
+  linux_sc7180_legacy = throw "'linux_sc7180_legacy' has been removed in favor of 'linux_latest'";
   warcraftlogs = throw "'warcraftlogs' has been removed in favor of 'archon-app'";
 }
