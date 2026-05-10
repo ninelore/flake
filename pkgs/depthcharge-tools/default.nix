e2cf7f5c7d91c25318f93a2c2962a2b1ce99923d
Ingo Reitz <9l@9lo.re>
2026-05-10 23:49:54 +0200

    pkgs: depthcharge-tools: switch to pmOS fork, 0.6.2 -> 0.6.4

--- pkgs/depthcharge-tools/default.nix
+++ pkgs/depthcharge-tools/default.nix
@@ -14,20 +14,25 @@
   zstd,
   ...
 }:
-with python3Packages;
-buildPythonApplication {
+python3Packages.buildPythonApplication (finalAttrs: {
   pname = "depthcharge-tools";
-  version = "0.6.2";
+  version = "0.6.4";
   src = fetchgit {
-    url = "https://codeberg.org/cabochon/depthcharge-tools";
-    hash = "sha256-3xPRNDUXLOwYy8quMfYSiBfzQl4peauTloqtZBGbvlw=";
+    url = "https://gitlab.postmarketos.org/postmarketOS/depthcharge-tools.git";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-McnBtc0UpatKO4XBnMOHf2L8xxcrsRM/5DCbmAmfA1o=";
   };
 
   nativeBuildInputs = [ makeWrapper ];
 
   pyproject = true;
-  build-system = [
+  build-system = with python3Packages; [
     setuptools
+  ];
+
+  dependencies = with python3Packages; [
+    importlib-metadata
+    packaging
   ];
 
   propagatedBuildInputs = [
@@ -36,7 +41,6 @@ buildPythonApplication {
     gzip
     lz4
     lzop
-    setuptools
     ubootTools
     vboot_reference
     xz
@@ -51,5 +55,4 @@ buildPythonApplication {
       "aarch64-linux"
     ];
   };
-
-}
+})
